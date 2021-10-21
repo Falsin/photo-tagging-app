0b3ef1cfb75cce066353caa674512d68fba4e35f
Falsin <greenldend@gmail.com>
2021-10-21 19:45:29 +0700

    added firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import React, { Component} from "react";
+import React, { useEffect, useState } from "react";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
-class App extends Component{
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNKAq1f1ZR2cHFpfvK8uQYSVDme3c3AY0",
+  authDomain: "photo-tagging-app-8d1c1.firebaseapp.com",
+  projectId: "photo-tagging-app-8d1c1",
+  storageBucket: "photo-tagging-app-8d1c1.appspot.com",
+  messagingSenderId: "1058241109815",
+  appId: "1:1058241109815:web:4c345126abaf01af52c4b2",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const storage = getStorage(firebaseApp);
+
+function App() {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    getDownloadURL(ref(storage, 'images/level-1.jpg'))
+      .then((url) => {
+        setImage(url);
+      })
+  }, [])
+
+  return (
+    <div className="App">
+      <h1> Hello, World! </h1>
+      <ImageComp src={image} />
+    </div>
+  )
+}
+
+function ImageComp(props) {
+  return <img src={props.src}></img>
+}
+
+/* class App extends Component {
+  
+
   render(){
     return(
       <div className="App">
-        <h1> Hello, World!!!! </h1>
+        <h1> Hello, World! </h1>
       </div>
     );
   }
-}
+} */
 
 export default App;
